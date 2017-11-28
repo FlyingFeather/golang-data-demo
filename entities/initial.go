a2dc9eabe814663e47dd6d6bb5f184b766bdca60
feather <1034080243@qq.com>
2017-11-28 09:54:32 +0800

    golang-data

---
+++ entities/initial.go
@@ -0,0 +1,24 @@
+package entities
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-xorm/xorm"
+)
+
+//var mydb *sql.DB
+var engine *xorm.Engine
+
+func init() {
+	// Init xorm engine
+	Engine, err := xorm.NewEngine("mysql", "root:root@tcp(127.0.0.1:3306)/test?charset=utf8&parseTime=true")
+	checkErr(err)
+	err = Engine.Sync2(new(UserInfo))
+	checkErr(err)
+	engine = Engine
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
